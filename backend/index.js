0984ca5739f88cfffaf2b0302b6778ecde2dfc95
robiulhasanofficial <robiul.hasan.info@gmail.com>
2025-05-02 23:43:10 +0600

    axcbvk

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vbdrzey.mongodb.net/chatDB?retryWrites=true&w=majority`;
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
